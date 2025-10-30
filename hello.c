a46f090990d0b595d2924bfd41b3eaecd4f44eca
Tymar144 <t144@umbc.edu>
2025-10-30 11:21:58 -0400

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-  printf("Hello Professor Kidd. My name is Tyler Martin NS I'm in your CMPE 311 class");
+  printf("Hello Professor Kidd. My name is Tyler Martin and I'm in your CMPE 311 class\n");
 
   return 0;
 }
